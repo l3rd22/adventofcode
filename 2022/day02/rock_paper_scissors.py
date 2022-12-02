88be91ca68b39946b5c78a58068f35b660c8d1f2
l3rd22 <freivodka+github@gmail.com>
2022-12-02 16:27:41 +0100

    unpacking in for loop

--- 2022/day02/rock_paper_scissors.py
+++ 2022/day02/rock_paper_scissors.py
@@ -1,13 +1,13 @@
 total_score = 0
 total_score_b = 0
 with open("input.txt") as strategy_guide:
-    for game_plan in (list(map(ord, line.split())) for line in strategy_guide):
-        symbol_score = game_plan[1] - ord("X") + 1
-        outcome_score = (game_plan[1] - game_plan[0] + 2) % 3 * 3
+    for opponents_move, response in (map(ord, line.split()) for line in strategy_guide):
+        symbol_score = response - ord("X") + 1
+        outcome_score = (response - opponents_move + 2) % 3 * 3
         total_score += symbol_score + outcome_score
 
-        symbol_score_b = ((game_plan[0] - ord("A")) + (game_plan[1] - ord("Y"))) % 3 + 1
-        outcome_score_b = (game_plan[1] - ord("X")) * 3 
+        symbol_score_b = ((opponents_move - ord("A")) + (response - ord("Y"))) % 3 + 1
+        outcome_score_b = (response - ord("X")) * 3 
         total_score_b += symbol_score_b + outcome_score_b
 print(total_score)
 print(total_score_b)

48171d70d2a76658834a11f01d33f84aa0002489
l3rd22 <freivodka+github@gmail.com>
2022-12-03 12:16:09 +0100

    using sets instead of list comprehension

--- 2022/day03/rucksack_reorganization.py
+++ 2022/day03/rucksack_reorganization.py
@@ -9,18 +9,16 @@ possible_badges = ["a"]
 sum_badge_priority = -1
 with open("input.txt", "r") as content_list:
     for elf_number, rucksack in enumerate(r.rstrip() for r in content_list):
-        item_type = [
-            itype
-            for itype in rucksack[: len(rucksack) // 2]
-            if itype in rucksack[len(rucksack) // 2 :]
-        ][0]
+        first_compartment = set(rucksack[: len(rucksack) // 2])
+        second_compartement = set(rucksack[len(rucksack) // 2 :])
+        item_type = list(first_compartment & second_compartement)[0]
         sum_priorities += priority(item_type)
 
         if elf_number % 3:
-            possible_badges = [itype for itype in possible_badges if itype in rucksack]
+            possible_badges = possible_badges & set(rucksack)
         else:
-            sum_badge_priority += priority(possible_badges[0])
-            possible_badges = rucksack
-    sum_badge_priority += priority(possible_badges[0])
+            sum_badge_priority += priority(list(possible_badges)[0])
+            possible_badges = set(rucksack)
+    sum_badge_priority += priority(list(possible_badges)[0])
 print(sum_priorities)
 print(sum_badge_priority)

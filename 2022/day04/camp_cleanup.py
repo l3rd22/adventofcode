941e33a71fd2b43dfd41e55205f158e72ac5f497
l3rd22 <freivodka+github@gmail.com>
2022-12-04 15:20:17 +0100

    single print(sum(...)) per part

--- 2022/day04/camp_cleanup.py
+++ 2022/day04/camp_cleanup.py
@@ -1,13 +1,21 @@
-containing_assignments = 0
-overlapping_assignments = 0
 with open("input.txt") as assignment_list:
-    for elf1, elf2 in (line.rstrip().split(",") for line in assignment_list):
-        elf1 = list(map(int, elf1.split("-")))
-        elf2 = list(map(int, elf2.split("-")))
-        if ((elf1[0]-elf2[0])*(elf1[1]-elf2[1])) <= 0:
-            containing_assignments += 1
-        if ((elf1[0]-elf2[1])*(elf1[1]-elf2[0])) <= 0:
-            overlapping_assignments += 1
-print(containing_assignments)
-print(overlapping_assignments)
+    print(
+        sum(
+            (elf1[0] - elf2[0]) * (elf1[1] - elf2[1]) <= 0
+            for elf1, elf2 in (
+                map(lambda elf: list(map(int, elf.split("-"))), elf_tuple)
+                for elf_tuple in (line.rstrip().split(",") for line in assignment_list)
+            )
+        )
+    )
 
+with open("input.txt") as assignment_list:
+    print(
+        sum(
+            (elf1[0] - elf2[1]) * (elf1[1] - elf2[0]) <= 0
+            for elf1, elf2 in (
+                map(lambda elf: list(map(int, elf.split("-"))), elf_tuple)
+                for elf_tuple in (line.rstrip().split(",") for line in assignment_list)
+            )
+        )
+    )

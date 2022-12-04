575b7904a62819d3fa278ea789583d9634d632a7
l3rd22 <freivodka+github@gmail.com>
2022-12-04 14:47:04 +0100

    added part two

--- 2022/day04/camp_cleanup.py
+++ 2022/day04/camp_cleanup.py
@@ -1,9 +1,13 @@
-number_bad_assignments = 0
+containing_assignments = 0
+overlapping_assignments = 0
 with open("input.txt") as assignment_list:
     for elf1, elf2 in (line.rstrip().split(",") for line in assignment_list):
         elf1 = list(map(int, elf1.split("-")))
         elf2 = list(map(int, elf2.split("-")))
         if ((elf1[0]-elf2[0])*(elf1[1]-elf2[1])) <= 0:
-            number_bad_assignments += 1
-print(number_bad_assignments)
+            containing_assignments += 1
+        if ((elf1[0]-elf2[1])*(elf1[1]-elf2[0])) <= 0:
+            overlapping_assignments += 1
+print(containing_assignments)
+print(overlapping_assignments)
 

91954d1fbcdbc676312f304cc8d5adb5236d0851
l3rd22 <freivodka+github@gmail.com>
2022-12-10 14:10:40 +0100

    FZBPBFZF

--- 2022/day10/cathode_ray_tube.py
+++ 2022/day10/cathode_ray_tube.py
@@ -7,11 +7,9 @@
     for xi in x
 )
 print(sum(X[i - 1] * i for i in range(20, 221, 40)))
-
-## PART TWO
-line = ""
-for cycle, sprite_pos in enumerate(X):
-    if cycle % 40 == 0:
-        print(line)
-        line = ""
-    line += [".", "#"][cycle % 40 - sprite_pos in (-1, 0, 1)]
+print(
+    "".join(
+        ["", "\n"][cycle % 40 == 0] + [".", "#"][cycle % 40 - sprite_pos in (-1, 0, 1)]
+        for cycle, sprite_pos in enumerate(X[:-1])
+    )
+)

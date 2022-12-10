d4935c5ca320c055a61c45813ec44b2d097ad6c0
l3rd22 <freivodka+github@gmail.com>
2022-12-10 13:45:09 +0100

    two lines should suffice

--- 2022/day10/cathode_ray_tube.py
+++ 2022/day10/cathode_ray_tube.py
@@ -1,7 +1,9 @@
-X = [1]
-with open("input.txt", "r") as instructions:
-    for instruction in instructions:
-        X.append(X[-1])
-        if instruction.startswith("addx"):
-            X.append(X[-1] + int(instruction[5:]))
+(X := [1]).extend(
+    xi
+    for x in (
+        (X[-1],) if instr.startswith("noop") else (X[-1], X[-1] + int(instr[5:]))
+        for instr in open("input.txt", "r")
+    )
+    for xi in x
+)
 print(sum(X[i - 1] * i for i in range(20, 221, 40)))

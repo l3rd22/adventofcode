e034b1064ff2a8d5c07e1a7f816b8873dff17485
l3rd22 <freivodka+github@gmail.com>
2022-12-18 15:16:44 +0100

    part two

--- 2022/day18/boiling_boulders.py
+++ 2022/day18/boiling_boulders.py
@@ -1,14 +1,42 @@
-positions = set()
+lava = set()
 neighbour_pairs = 0
 with open("input.txt", "r") as input:
     for line in input:
-        positions.add(pos := tuple(map(int, line.rstrip().split(","))))
+        lava.add(pos := tuple(map(int, line.rstrip().split(","))))
         neighbour_pairs += sum(
-            adj in positions
-            for adj in (
+            nb in lava
+            for nb in (
                 tuple(pos[i] + (j == i) * k for i in range(3))
                 for j in range(3)
                 for k in (-1, 1)
             )
         )
-print(6 * len(positions) - 2 * neighbour_pairs)
+print(6 * len(lava) - 2 * neighbour_pairs)
+
+## PART TWO
+upper_limits = tuple(max(pos[i] for pos in lava) + 2 for i in range(3))
+steam = set()
+current = set((upper_limits,))
+while current:
+    steam.update(current)
+    current = set(
+        nb
+        for nb in (
+            tuple((pos[i] + (j == i) * k) % upper_limits[i] for i in range(3))
+            for pos in current
+            for j in range(3)
+            for k in (-1, 1)
+        )
+        if nb not in steam and nb not in lava
+    )
+print(
+    sum(
+        nb in lava
+        for nb in (
+            tuple((pos[i] + (j == i) * k) % upper_limits[i] for i in range(3))
+            for pos in steam
+            for j in range(3)
+            for k in (-1, 1)
+        )
+    )
+)

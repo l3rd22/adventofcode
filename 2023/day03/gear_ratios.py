6f74841c4f9c961255ef8b843fd84cce3f6a292b
l3rd22 <freivodka+github@gmail.com>
2024-12-06 20:29:55 +0100

    ADD: 2023 day03 part 2

--- 2023/day03/gear_ratios.py
+++ 2023/day03/gear_ratios.py
@@ -7,19 +7,27 @@ def main():
     with open("input.txt", "r") as fobj:
         schematic = fobj.read()
     ncols = schematic.index("\n") + 1
+    schematic = f"\n{'.' * (ncols - 1)}\n" + schematic
     total = 0
+    gears = {}
     for match in re.finditer(r"\d+", schematic):
         adjacent = "".join(
             schematic[
                 slice(
-                    max(match.span()[0] - 1 + offset, 0),
-                    max(match.span()[1] + 1 + offset, 0),
+                    match.span()[0] - 1 + offset,
+                    match.span()[1] + 1 + offset,
                 )
             ]
             for offset in (-ncols, 0, ncols)
         )
         total += int(match[0]) if re.search(r"[^\.\d\n]", adjacent) is not None else 0
+        for gear in re.finditer(r"\*", adjacent):
+            row, col = divmod(gear.span()[0], len(match[0]) + 2)
+            gears.setdefault(match.span()[0] - 1 + col + (row - 1) * ncols, []).append(
+                int(match[0])
+            )
     print(total)
+    print(sum(val[0] * val[1] if len(val) == 2 else 0 for val in gears.values()))
 
 
 if __name__ == "__main__":

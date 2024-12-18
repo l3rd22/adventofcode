2caa92c1a66b6df0c01b1506986ead00185cf6a7
l3rd22 <freivodka+github@gmail.com>
2024-12-18 12:40:28 +0100

    MAINT: refactor verbose functionality into run()

--- 2024/day17/chronospatial_computer.py
+++ 2024/day17/chronospatial_computer.py
@@ -5,88 +5,59 @@ import sys
 from contextlib import redirect_stdout
 
 
-def adv(operand, verbose=False):
+def adv(operand):
     global A
     res = A >> combo(operand)
     A = res
-    if verbose:
-        sys.stdout.write(
-            f"A <- A >> {combo_verbose.get(operand, f'{operand:02d}')}\tA: {res:02d}"
-        )
     return res
 
 
-def bxl(operand, verbose=False):
+def bxl(operand):
     global B
     res = B ^ operand
     B = res
-    if verbose:
-        sys.stdout.write(f"B <- B xor {operand:02d}\tB: {res:02d}")
     return res
 
 
-def bst(operand, verbose=False):
+def bst(operand):
     global B
     res = combo(operand) & 0b111
     B = res
-    if verbose:
-        sys.stdout.write(
-            f"B <- {combo_verbose.get(operand, f'{operand:02d}')} mod 08\tB: {res:02d}"
-        )
     return res
 
 
-def jnz(operand, verbose=False):
+def jnz(operand):
     global ip
     if not A:
-        if verbose:
-            sys.stdout.write("A =/= 00?\tNOP")
         return ip + 2
     ip = operand - 2
-    if verbose:
-        sys.stdout.write(f"A =/= 00?\tJMP {operand:02d}")
     return operand
 
 
-def bxc(operand, verbose=False):
+def bxc(operand):
     global B
     res = B ^ C
     B = res
-    if verbose:
-        sys.stdout.write(f"B <- B xor C\tB: {res:02d}")
     return res
 
 
-def out(operand, verbose=False):
+def out(operand):
     res = combo(operand) & 0b111
-    if verbose:
-        sys.stdout.write(
-            f"OUT {combo_verbose.get(operand, f'{operand:02d}')} mod 08\tOUT {res:02d}"
-        )
-    else:
-        sys.stdout.write(f"{res},")
+    sys.stdout.write(f"{res},")
     return res
 
 
-def bdv(operand, verbose=False):
+def bdv(operand):
     global B
     res = A >> combo(operand)
     B = res
-    if verbose:
-        sys.stdout.write(
-            f"B <- A >> {combo_verbose.get(operand, f'{operand:02d}')}\tB: {res:02d}"
-        )
     return res
 
 
-def cdv(operand, verbose=False):
+def cdv(operand):
     global C
     res = A >> combo(operand)
     C = res
-    if verbose:
-        sys.stdout.write(
-            f"C <- A >> {combo_verbose.get(operand, f'{operand:02d}')}\tC: {res:02d}"
-        )
     return res
 
 
@@ -108,14 +79,18 @@ def run(program, verbose=False):
     global ip
     ip = 0
     while ip < len(program):
+        ipv = f"{ip:02d}"
         opcode, operand = program[ip : ip + 2]
-        if verbose:
-            print(
-                f"\n{ip:02d}\t{op[opcode].__name__} {operand:02d}",
-                end="\t\t",
-            )
-        op[opcode](operand, verbose)
+        res = op[opcode](operand)
         ip += 2
+        if verbose:
+            instr = f"{op[opcode].__name__} {operand:02d}"
+            info = opinfo[opcode].format(
+                literalop=f"{operand:02d}",
+                comboop=comboinfo.get(operand, f"{operand:02d}"),
+                res=f"{res:02d}",
+            )
+            print(f"\r{ipv}\t{instr}\t\t{info}")
 
 
 def disassemble(program):
@@ -128,11 +103,6 @@ def disassemble(program):
     return f"{header}\n{body}\n{footer}"
 
 
-combo_verbose = {
-    4: "A",
-    5: "B",
-    6: "C",
-}
 op = {
     0: adv,
     1: bxl,
@@ -142,6 +112,22 @@ op = {
     5: out,
     6: bdv,
     7: cdv,
+}
+
+opinfo = {
+    0: "A <- A >> {comboop}\tA: {res}",
+    1: "B <- B xor {literalop}\tB: {res}",
+    2: "B <- {comboop} mod 08\tB: {res}",
+    3: "A =/= 00?\tJMP {res}",
+    4: "B <- B xor C\tB: {res}",
+    5: "OUT {comboop} mod 08\tOUT {res}",
+    6: "B <- A >> {comboop}\tB: {res}",
+    7: "C <- A >> {comboop}\tC: {res}",
+}
+comboinfo = {
+    4: "A",
+    5: "B",
+    6: "C",
 }
 
 
@@ -157,9 +143,9 @@ def main(verbose=False):
 
     if verbose:
         print(disassemble(program), end="\n\n")
-        print(f"Register A: {A}\nRegister B: {B}\nRegister C: {C}")
+        print(f"Register A: {A}\nRegister B: {B}\nRegister C: {C}", end="\n\n")
     run(program, verbose)
-    print("\n" if verbose else "\b ")
+    print("" if verbose else "\b ")
 
     program_str = ",".join(map(str, program))
     A_copy = 0
